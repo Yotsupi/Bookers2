708fad526a134cb922ef0b4631e20eb624645c2f
Yotsupi <kayoshida.1830@gmail.com>
2021-09-11 05:09:42 +0000

    [Fix]レイアウト微調整

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '07868bba3d588662bef19220a2608fc672c61695305699eee24faa5f1c21887826b66e77287c8ee9f9b7839ae7d9a83939a4b60463f2c6c903ca6aff1afa4d75'
